d52370c8cbc6fa35226a23e5729274c065b0089a
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-21 14:15:04 -0600

    first look to express

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const express = require('express');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  const path = require('path');
-  app.get('*', (req, res)  => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  });
-}
+app.get('/', (req, res) => {
+  res.send({
+    hello: 'ok'
+  })
+})
 
 const PORT = process.env.PORT || 3001;
 
