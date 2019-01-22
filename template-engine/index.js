e214dd5f1fa61f6fcc450f921294ad9fb234b426
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-21 18:52:55 -0600

    let's go to the ecommerce app

--- template-engine/index.js
+++ template-engine/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const expressJsx = require('./express-jsx');
 
-app.engine('jsx', expressJsx);
-app.set('views', './views');
-app.set('view engine', 'jsx');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('index', { hello: 'hola', world: 'mundo' })
-});
+app.get('/products', productsRouter);
 
 const server = app.listen(8000, () => {
   console.log(`Listening http://localhost:${server.address().port}`)
