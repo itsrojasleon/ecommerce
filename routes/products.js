91eb060b820183aecf3b9f940708293b50fa1cb2
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-22 14:52:18 -0600

    Layout with pug

--- routes/products.js
+++ routes/products.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const  products = [
-  { name: "Red shoes", price: 75 },
-  { name: "Red shoes", price: 75 },
-  { name: "Red shoes", price: 75 }
+  { name: "Red shoes", price: 75, image: "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0e44599932dce6b8440e26fb91e10a69&auto=format&fit=crop&w=800&q=60" },
+  { name: "Red shoes", price: 75, image: "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0e44599932dce6b8440e26fb91e10a69&auto=format&fit=crop&w=800&q=60" },
+  { name: "Red shoes", price: 75, image: "https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0e44599932dce6b8440e26fb91e10a69&auto=format&fit=crop&w=800&q=60" },
 ];
 router.get('/', (req, res) => {
   res.render('products', { products })
